f38655da49260f8a14c82066618c8e631e85b924
yusufgozukara <yusufgozukara11@gmail.com>
2022-08-29 08:49:16 +0300

    html's linked

---
+++ student/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from .views import student_page
+
+urlpatterns = [
+
+    path('', student_page, name='student'),
+]
--- student/views.py
+++ student/views.py
@@ -1,3 +1,9 @@
+
+
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'student/index.html')
+
+def student_page(request):
+    return render(request,'student/student.html')
