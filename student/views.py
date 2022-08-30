3aa9693fdfdf5d552e3926c39925db9b26fd154d
yusufgozukara <yusufgozukara11@gmail.com>
2022-08-30 14:07:08 +0300

    form linked to html

--- student/views.py
+++ student/views.py
@@ -1,9 +1,20 @@
 
 
+from multiprocessing import context
 from django.shortcuts import render
+
+from .forms import StudentForm
 
 def index(request):
     return render(request, 'student/index.html')
 
+# def student_page(request):
+#     return render(request,'student/student.html')
+
 def student_page(request):
-    return render(request,'student/student.html')
+    form = StudentForm()
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'student/student.html', context)
